45e8395603a3b9e44407c3700579e507971c0550
lamia <Lamia.Omar.ruh.k@tuwaiq.edu.sa>
2021-12-31 19:47:18 +0300

    last-commit last commit before merge to develop

--- app/src/main/java/com/example/gbook/BookViewmodel.kt
+++ app/src/main/java/com/example/gbook/BookViewmodel.kt
@@ -69,6 +69,7 @@ class BookViewmodel(
                     }
                     return@withContext list
 
+
                 }
                 _bookCategoryResultUi.update { it.copy(categoryList = data) }
 
